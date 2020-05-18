58beeeb724211dc18efd586824fbb00cef334ac6
majung2 <dlalwjd1417@likelion.org>
2020-05-18 16:18:19 +0900

    boj_1966

--- python/boj/boj_2798.py
+++ python/boj/boj_2798.py
@@ -29,7 +29,6 @@ for i in range(0,length):
     for j in range(i+1,length):
         for k in range(j+1,length):
             sum_value = data[i]+data[j]+data[k]
-            print(i,',',j,',',k)
             if sum_value <= m:
                 result = max(result,sum_value)
 print(result)
