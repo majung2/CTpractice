8ac175df80a06cea407b4a9849a09ab073501c53
majung2 <dlalwjd1417@likelion.org>
2020-05-18 17:07:50 +0900

    boj_5397

---
+++ python/boj/boj_5397.py
@@ -0,0 +1,29 @@
+# 키로거
+# 스택, 구현, 그리디
+# 41 / 40분
+
+# 시간초과! > 스택 2개로 해결
+
+test_case = int(input())
+
+for _ in range(test_case):
+    L = input()
+    left = []
+    right = []
+    for key in L:
+        if key == '<':
+            if left:
+                right.append(left.pop())
+        elif key == '>':
+            if right:
+                left.append(right.pop())
+        elif key == '-':
+            if left:
+                left.pop()
+        else:
+            left.append(key)
+
+    # left.extend(reversed(right))
+    while right: 
+        left.append(right.pop())
+    print(''.join(left))
