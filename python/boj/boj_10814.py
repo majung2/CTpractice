7f9eed3925921aa2cfa40323e7fd58cf55fd8cdd
majung2 <dlalwjd1417@likelion.org>
2020-05-19 21:25:31 +0900

    boj_10814

---
+++ python/boj/boj_10814.py
@@ -0,0 +1,33 @@
+# 나이순 정렬
+# 하
+# 정렬
+# 18 / 15분
+
+N = int(input())
+result = []
+
+for _ in range(N):
+    age, name = map(str, input().split())
+    age=int(age)
+    result.append((age,name))
+
+result = sorted(result, key=lambda person: person[0])
+
+for i in result:
+    print(i[0],i[1])
+
+
+# 인강 풀이
+# n = int(input())
+# array = []
+
+# for _ in range(n):
+#     input_data = input().split()
+#     array.append((int(input_data[0]),input_data[1]))
+
+# array = sorted(array, key=lambda x:x[0])
+
+# for i in array:
+#     print(i[0],i[1])
+    
+        
