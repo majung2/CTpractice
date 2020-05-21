86a688c73bf5827eb252dc809170337fb9d0e4f4
majung2 <dlalwjd1417@likelion.org>
2020-05-21 13:29:48 +0900

    Boj_1542.java

---
+++ boj/Boj_1542.java
@@ -0,0 +1,36 @@
+// 문서 검색
+// 하
+// 탐색
+// 25 / 20분 
+
+package boj;
+
+import java.util.Scanner;
+
+public class Boj_1542 {
+    public static void main(String[] args) {
+        Scanner scin = new Scanner(System.in);
+        String doc = scin.nextLine();
+        String word = scin.nextLine();
+        scin.close();
+
+        int result = 0;
+        int count = 0;
+        int len = word.length();
+
+        for (int j = 0; j <= doc.length() - len; j++) {
+            count = 0;
+            for (int i = j; i <= doc.length() - len;) {
+                if (doc.substring(i, i + len).equals(word)) {
+                    count++;
+                    i += len;
+                } else {
+                    i++;
+                }
+            }
+            result = (result > count) ? result : count;
+        }
+        System.out.println(result);
+    }
+
+}
